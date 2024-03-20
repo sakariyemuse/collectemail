c28c0cfed18ad6236807dd25617f1aef3e04947e
sakariyemuse <148655505+sakariyemuse@users.noreply.github.com>
2024-03-20 11:26:09 +0800

    Update thankyou.js

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -1,12 +1,17 @@
-// pages/thankyou.js
+import Link from 'next/link';
 
 export default function ThankYouPage() {
-    return (
+  return (
+    <div className="flex justify-center items-center h-screen">
       <div className="text-center">
-        <h1>Thank You for Subscribing!</h1>
-        <p>We appreciate your interest. You will receive updates via email.</p>
-        <a href="/" className="text-blue-500 hover:underline">Go back to homepage</a>
+        <h1 className="text-3xl font-bold mb-4">Thank You for Subscribing!</h1>
+        <p className="text-lg mb-4">
+          We appreciate your interest. You'll receive updates via email soon.
+        </p>
+        <Link href="/">
+          <button className="text-blue-500 hover:underline">Back to Home</button>
+        </Link>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
